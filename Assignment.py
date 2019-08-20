086df65a08f4271d18a45855fae181b959074a13
Liu,Song <liudsl@126.com>
2019-08-21 01:27:23 +1200

    完成初步数据预处理

--- Assignment.py
+++ Assignment.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+
 data = np.loadtxt("bank.csv", dtype=np.str, delimiter=";", skiprows=1)
 
-print(data)
+
+def remove_extra_quotes(para_data):
+    # Remove the extra double quotes
+    # return null
+    for row in para_data:
+        for column_idx in range(len(row)):
+            row[column_idx] = row[column_idx].replace("\"", "")
+
+
+remove_extra_quotes(para_data=data)
